838e6d4015c99653814f4d4788117361a4e29cab
emna-jridi <Emna.JRIDI@esprit.tn>
2025-02-12 20:02:51 +0100

    Connexion à la base de données

---
+++ src/main/java/tn/esprit/test/Main.java
@@ -0,0 +1,9 @@
+package tn.esprit.test;
+
+import tn.esprit.utils.MyDatabase;
+
+public class Main {
+    public static void main(String[] args) {
+        MyDatabase bd = MyDatabase.getInstance();
+    }
+}
